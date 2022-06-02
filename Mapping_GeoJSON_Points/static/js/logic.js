3fd3d436a7e10e2194d8324c551a98f8500d5aa9
kntln <karennikki.tolentino@gmail.com>
2022-06-01 22:30:28 -0400

    Adding updated logic.js

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -4,36 +4,17 @@ console.log("working");
 // Create the map object with center and zoom level.
 let map = L.map('mapid').setView([30, 30], 2);
 
-// Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/kntln/Mapping_Earthquakes/Mapping_GeoJSON_Points/Mapping_GeoJSON_Points/static/js/majorAirports.json";
 
 
-  // Grabbing our GeoJSON data.
- L.geoJson(sanFranAirport, {
-  // We turn each feature into a marker on the map.
-     onEachFeature: function(feature, layer) {
-         console.log(layer);
-         layer.bindPopup("<h2>" + feature.properties.name + "</h2>" + "<hr>" 
-         + "<h2>" + feature.properties.city + ", " + feature.properties.country + "</h2>")
-     }
-    }).addTo(map);
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
+});
    
     
 
